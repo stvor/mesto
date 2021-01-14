8bf5ea99bd3de3033a62bf8601f903c8273abac7
Stas Voronin <jameslebron@yandex.ru>
2021-01-14 20:27:44 +0300

    fix: исправил название модификатора для открытия попапа по ТЗ

--- scripts/index.js
+++ scripts/index.js
@@ -2,5 +2,5 @@ let profileEditButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 
 profileEditButton.addEventListener('click', () => {
-  popup.classList.add('popup_active');
+  popup.classList.add('popup_open');
 });
