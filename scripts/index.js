f861cbfe6b23fe8e5470e68b8ea3b1ad5336cc82
Stas Voronin <jameslebron@yandex.ru>
2021-01-14 20:13:11 +0300

    feat: сделал открытие попапа

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+
+profileEditButton.addEventListener('click', () => {
+  popup.classList.add('popup_active');
+});
