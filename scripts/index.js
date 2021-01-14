c94d3fcb043f592057345a8137d59228dc39323a
Stas Voronin <jameslebron@yandex.ru>
2021-01-14 20:28:22 +0300

    feat: добавил закрытие попапа по крестику

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 let profileEditButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
+let popupCloseButton = popup.querySelector('.popup__close');
 
 profileEditButton.addEventListener('click', () => {
   popup.classList.add('popup_open');
+});
+
+popupCloseButton.addEventListener('click', () => {
+  popup.classList.remove('popup_open');
 });
