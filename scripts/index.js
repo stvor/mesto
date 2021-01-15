bf9e04697e3e9b7d8526386fbbe5da0911b813f2
Stas Voronin <jameslebron@yandex.ru>
2021-01-15 21:09:49 +0300

    feat: подтянул в форму имя и профессию из профиля

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,15 @@
 let profileEditButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = popup.querySelector('.popup__close');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+let nameInput = document.querySelector('.form__name');
+let professionInput = document.querySelector('.form__profession');
 
 profileEditButton.addEventListener('click', () => {
   popup.classList.add('popup_open');
+  nameInput.value = profileName.textContent;
+  professionInput.value = profileProfession.textContent;
 });
 
 popupCloseButton.addEventListener('click', () => {
