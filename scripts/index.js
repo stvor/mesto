06e95de03f7191f9624e271dc239477ba2becde1
Stas Voronin <jameslebron@yandex.ru>
2021-01-28 14:42:59 +0300

    feat: добавил данные первых карточек в JS

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,33 @@ let professionInput = document.querySelector('.form__profession');
 let formElement = document.querySelector('.form');
 let likeButtons = document.querySelectorAll('.cards-grid__like-button');
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 function popupOpen() {
   popup.classList.add('popup_open');
   nameInput.value = profileName.textContent;
