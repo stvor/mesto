0cdb6f59eb3056109c6280101f647188ba411de2
Stas Voronin <jameslebron@yandex.ru>
2021-02-02 22:13:05 +0300

    style: добавил комментарии в код скрипта

--- scripts/index.js
+++ scripts/index.js
@@ -19,16 +19,24 @@ const placeImage = placeImagePopup.querySelector('.image-popup__place-image');
 const placeName = placeImagePopup.querySelector('.image-popup__place-name');
 const closePlaceImagePopupButton = placeImagePopup.querySelector('.image-popup__close');
 
+
+////////////////////////////////////////////////////////////
+// ОТРИСОВКА КАРТОЧЕК
+////////////////////////////////////////////////////////////
+
+// Отрисовываем первые карточки
 function renderInitialCards () {
   initialCards.reverse().forEach((card) => {
     renderCard (card, cardsList);
   });
 }
 
+// Отрисовываем одну карточку
 function renderCard (data, wrap) {
   wrap.prepend(createCard(data));
 }
 
+// Создаем одну карточку
 function createCard (cardData) {
   const cardItem = cardTemplate.cloneNode(true);
 
@@ -48,14 +56,27 @@ function createCard (cardData) {
   return cardItem;
 }
 
+
+////////////////////////////////////////////////////////////
+// УДАЛЕНИЕ КАРТОЧЕК
+////////////////////////////////////////////////////////////
+
 function handleDeleteCard (evt) {
   evt.target.closest('.cards-grid__list-item').remove();
 }
+
+
 
 function handleLikeCard (evt) {
   evt.target.classList.toggle('cards-grid__like-button_active');
 }
 
+
+////////////////////////////////////////////////////////////
+// РАБОТА С ПОПАПАМИ
+////////////////////////////////////////////////////////////
+
+// Любой попап
 function openPopup(popup) {
   popup.classList.add('popup_open');
 }
@@ -64,6 +85,7 @@ function closePopup(popup) {
   popup.classList.remove('popup_open');
 }
 
+// Попап редактирования профиля
 function handleProfileEditPopupOpen() {
   openPopup(profileEditPopup);
 
@@ -84,6 +106,7 @@ function handleProfileEditFormSubmit (evt) {
   handleProfileEditPopupClose();
 }
 
+// Попап добавления новой карточки
 function handleCardAddPopupOpen() {
   openPopup(cardAddPopup);
 }
@@ -108,6 +131,7 @@ function handleCardAddFormSubmit (evt) {
   handleCardAddPopupClose();
 }
 
+// Попап изображения
 function handlePlaceImagePopupOpen(cardData) {
   placeImage.src = cardData.link;
   placeImage.alt = cardData.name;
@@ -120,14 +144,28 @@ function handlePlaceImagePopupClose() {
   closePopup(placeImagePopup);
 }
 
+
+////////////////////////////////////////////////////////////
+// ДОБАВЛЕНИЕ СЛУШАТЕЛЕЙ
+////////////////////////////////////////////////////////////
+
+// Попап добавления новой карточки
 cardAddButton.addEventListener('click', handleCardAddPopupOpen);
 closeCardAddPopupButton.addEventListener('click', handleCardAddPopupClose);
 cardAddFormElement.addEventListener('submit', handleCardAddFormSubmit);
 
+// Попап редактирования профиля
 profileEditButton.addEventListener('click', handleProfileEditPopupOpen);
 closeProfileEditPopupButton.addEventListener('click', handleProfileEditPopupClose);
 profileEditFormElement.addEventListener('submit', handleProfileEditFormSubmit);
 
+// Попап изображения
 closePlaceImagePopupButton.addEventListener('click', handlePlaceImagePopupClose);
 
+
+////////////////////////////////////////////////////////////
+// ВЫЗОВ ФУНКЦИЙ
+////////////////////////////////////////////////////////////
+
+// Отрисовать первые карточки
 renderInitialCards();
