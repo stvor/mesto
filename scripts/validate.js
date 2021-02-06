caee4664531ea2dcf4a60f07175a99752e380f56
Stas Voronin <jameslebron@yandex.ru>
2021-02-06 15:01:46 +0300

    feat: заменил текст ошибки на браузерный

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,19 +2,21 @@ const formElement = document.querySelector('.form');
 const formInput = formElement.querySelector('.form__input');
 const formError = formElement.querySelector(`.${formInput.id}-error`);
 
-const showInputError = (element) => {
+const showInputError = (element, errorMessage) => {
   element.classList.add('form__input_type_error');
+  formError.textContent = errorMessage;
   formError.classList.add('form__input-error_active');
 };
 
 const hideInputError = (element) => {
   element.classList.remove('form__input_type_error');
   formError.classList.remove('form__input-error_active');
+  formError.textContent = '';
 };
 
 const isValid = () => {
   if (!formInput.validity.valid) {
-    showInputError(formInput);
+    showInputError(formInput, formInput.validationMessage);
   } else {
     hideInputError(formInput);
   }
