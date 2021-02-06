620ddfa447bdf075f7c469f04b46d10a0277fbbc
Stas Voronin <jameslebron@yandex.ru>
2021-02-06 14:57:33 +0300

    feat: появляется ошибка валидации

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,12 +1,15 @@
 const formElement = document.querySelector('.form');
 const formInput = formElement.querySelector('.form__input');
+const formError = formElement.querySelector(`.${formInput.id}-error`);
 
 const showInputError = (element) => {
   element.classList.add('form__input_type_error');
+  formError.classList.add('form__input-error_active');
 };
 
 const hideInputError = (element) => {
   element.classList.remove('form__input_type_error');
+  formError.classList.remove('form__input-error_active');
 };
 
 const isValid = () => {
