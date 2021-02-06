3bbabbe4b6a374ee478d0b95e64a9a20b944cce4
Stas Voronin <jameslebron@yandex.ru>
2021-02-06 14:19:10 +0300

    feat: невалидный инпут становится красным

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,1 +1,24 @@
-console.log(123);
+const formElement = document.querySelector('.form');
+const formInput = formElement.querySelector('.form__input');
+
+const showInputError = (element) => {
+  element.classList.add('form__input_type_error');
+};
+
+const hideInputError = (element) => {
+  element.classList.remove('form__input_type_error');
+};
+
+const isValid = () => {
+  if (!formInput.validity.valid) {
+    showInputError(formInput);
+  } else {
+    hideInputError(formInput);
+  }
+};
+
+formElement.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+});
+
+formInput.addEventListener('input', isValid);
