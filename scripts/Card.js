04508a69b80194aa35aa7c321294adb296492829
Stas Voronin <jameslebron@yandex.ru>
2021-03-04 11:26:47 +0300

    feat: добавил класс карточки

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,3 +1,40 @@
+const cardsData = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
 export default class Card {
+  constructor(cardsData, cardSelector) {
+    this._cardSelector = cardSelector;
+  }
+
+  _getTemplate() {}
+
+  _setEventListeners() {}
+
+// обработчики слушателей
 
 }
