c2841739fc2279e56bee92613ed01501ba1720f1
Stas Voronin <jameslebron@yandex.ru>
2021-03-17 23:01:41 +0300

    feat: добавил в webpack autoprefixer и минификацию css

--- webpack.config.js
+++ webpack.config.js
@@ -31,8 +31,12 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader'
-        }]
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
+        'postcss-loader']
       }
     ]
   },
