af0d43b35bab71cdd1bc70b790b556c100a76ce9
Stas Voronin <jameslebron@yandex.ru>
2021-03-17 22:37:09 +0300

    feat: научил webpack работать с html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -22,5 +23,10 @@ module.exports = {
         exclude: '/node_modules/'
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ]
 }
