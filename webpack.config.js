c8e3bdb2af7632f067a82c953fa60ad1b0590e46
Stas Voronin <jameslebron@yandex.ru>
2021-03-21 20:06:23 +0300

    fix: починил подключение картинок в html для вебпака

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-      publicPath: ''
+    publicPath: ''
   },
   mode: 'development',
   devServer: {
