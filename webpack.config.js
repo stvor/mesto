833c1dacf9c3db0349293ad24fd4016a65018dea
Stas Voronin <jameslebron@yandex.ru>
2021-03-22 14:37:04 +0300

    feat: добавил папку src

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './scripts/index.js',
+  entry: './src/scripts/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -50,7 +50,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './index.html')
+      template: path.resolve(__dirname, './src/index.html')
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
