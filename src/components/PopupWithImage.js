41e70851160a55bb45435d8ca573c72df5d50248
Stas Voronin <jameslebron@yandex.ru>
2021-03-24 11:14:46 +0300

    refactor: вынес поиск констант в конструктор PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,12 +3,11 @@ import { Popup } from './Popup.js';
 export class PopupWithImage extends Popup {
   constructor( popupSelector ) {
     super(popupSelector);
+    this._placeImage = this._popup.querySelector('.image-popup__place-image');
+    this._placeName = this._popup.querySelector('.image-popup__place-name');
   }
 
   open(cardData) {
-    this._placeImage = this._popup.querySelector('.image-popup__place-image');
-    this._placeName = this._popup.querySelector('.image-popup__place-name');
-
     this._placeImage.src = cardData.link;
     this._placeName.alt = cardData.name;
     this._placeName.textContent = cardData.name;
